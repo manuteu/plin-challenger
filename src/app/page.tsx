a912e01fe3dc542908b8c8fbab309e0a6d8b61b8
Matheus <matheusim27@hotmail.com>
2023-08-28 09:53:56 -0300

    add: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-'use client'
-import { GetWeather } from "./components/GetWeather";
-
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
